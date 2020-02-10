7268bac2d54bd6aa9849ab91076acf5c43e382dd
nikki <cuapopomama@gmail.com>
2020-02-10 16:02:49 +0800

    士兵動畫補充

---
+++ Assets/Scripts/ArmMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ArmMove : MonoBehaviour
+{
+    public Vector3 rightFace;
+    public Vector3 leftFace;
+    public int posType;
+
+    public float duration = 10;
+    float gap = 8;
+    void Start()
+    {
+        rightFace = transform.localScale;
+        leftFace = new Vector3(-rightFace.x, rightFace.y, rightFace.z);
+
+        if (posType == 0)
+        {
+            Sequence mySequence = DOTween.Sequence();
+
+            mySequence.Append(transform.DOScale(leftFace, 0));
+            mySequence.Append(transform.DOLocalMoveX(-gap, duration).SetEase(Ease.Linear));
+            mySequence.Append(transform.DOScale(rightFace, 0));
+            mySequence.Append(transform.DOLocalMoveX(gap, duration).SetEase(Ease.Linear));
+
+            mySequence.SetLoops(-1);
+        }
+
+        if (posType == 1)
+        {
+            Sequence mySequence = DOTween.Sequence();
+
+            mySequence.Append(transform.DOScale(rightFace, 0));
+            mySequence.Append(transform.DOLocalMoveX(gap, duration).SetEase(Ease.Linear));
+            mySequence.Append(transform.DOScale(leftFace, 0));
+            mySequence.Append(transform.DOLocalMoveX(-gap, duration).SetEase(Ease.Linear));
+
+            mySequence.SetLoops(-1);
+        }
+
+        if (posType == 2)
+        {
+            Sequence mySequence = DOTween.Sequence();
+
+            mySequence.Append(transform.DOScale(rightFace, 0));
+            mySequence.Append(transform.DOLocalMoveX(gap, duration / 2).SetEase(Ease.Linear));
+            mySequence.Append(transform.DOScale(leftFace, 0));
+            mySequence.Append(transform.DOLocalMoveX(-gap, duration).SetEase(Ease.Linear));
+            mySequence.Append(transform.DOScale(rightFace, 0));
+            mySequence.Append(transform.DOLocalMoveX(0, duration / 2).SetEase(Ease.Linear));
+
+            mySequence.SetLoops(-1);
+        }
+    }
+}
---
+++ Assets/Scripts/QuitApp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitApp : MonoBehaviour
+{
+    public int count = 0;
+    public int affectCount = 5;
+    public Button BTNQuit;
+    void Start()
+    {
+        BTNQuit.onClick.AddListener(DoCountQuit);
+    }
+
+    void DoCountQuit(){
+        count++;
+
+        if(count > affectCount)
+            Application.Quit();
+    }
+}
